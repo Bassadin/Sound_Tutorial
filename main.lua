c24b1414a301cbc06bcc78fbeb4af2a07d2bc1df
Bastian Hodapp <bastianhodapp@gmail.com>
2014-10-06 14:34:16 +0200

    Comments
    Added two lines of comments to make the code more understandable

--- main.lua
+++ main.lua
@@ -1,9 +1,11 @@
 la = love.audio
 
 function love.load()
+  --Loads the sound file into our code
   beep = love.audio.newSource("lazor.wav", "static")
 end
 
 function love.keypressed()
+  --Simply plays the sound file whenever the keyboard is pressed
   love.audio.play(beep)
 end
